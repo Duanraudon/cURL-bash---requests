eb02d56a5f7e4aa192601deef917f88cc6e0e83e
Duanraudon <34836995+Duanraudon@users.noreply.github.com>
2018-09-25 13:26:47 +0800

    Update cURL(bash)自动生成request请求.py

--- cURL(bash)自动生成request请求.py
+++ cURL(bash)自动生成request请求.py
@@ -2,30 +2,38 @@ import re
 
 def dealwith_cookies(r):
     #re正则选取cookie字段
-    cookie = re.findall('cookie.*?(?:\' )|Cookie.*?(?:\' )',r)
+    cookie = re.findall('cookie.*?(?:\' )|Cookie.*?(?:\' )|cookie.*?(?:\')',r)
     #数据清洗打印
-    cookie = eval(str(cookie).strip("[]")).strip(" ")
-    cookie = cookie[:-1].split(": ")
-    cookie = cookie[1].split("; ")
-    print("cookies = {")
-    for i in cookie:
-        l = str(i.split("=",1)).strip("[]").replace(", ",": ",1)+','
-        print(l)
-    print("}\n")
-
+    try:
+        if cookie != '[]':
+            cookie = eval(str(cookie).strip("[]")).strip(" ")
+            cookie = cookie[:-1].split(": ")
+            cookie = cookie[1].split("; ")
+            print("cookies = {")
+            for i in cookie:
+                l = str(i.split("=",1)).strip("[]").replace(", ",": ",1)+','
+                print(l)
+            print("}\n")
+            return ' '
+    except:return '[]'
 
 def dealwith_data(r,datas):
+
     #re正则选取data字段
     data = re.findall('data.*',r)
     #如果data字段不为[]则进行数据清理与打印
-    if data!=[]:
-        data = str(data).strip("[]' ").replace('"','').split(' ')
-        data = str(data[1]).split("^&")
-        for i in data:
-            t = str(i.split("=")).strip("[]")
-            datas.append(eval(t))
-        data = "data = " + str(datas)
-        print(data)
+    try:
+        if data != '[]':
+            data = str(data).strip("[]' ").replace('"','').split(' ')
+            data = str(data[1]).split("^&")
+            for i in data:
+                t = str(i.split("=")).strip("[]")
+                datas.append(eval(t))
+            data = "data = " + str(datas)
+            print(data)
+
+    except:
+        return '[]'
 #关键字函数，进行headers的各个字段的筛选与清理。
 def dealwith_parameter(parameter,r):
     #可能存在某些网页headers的字段是小写情况，所以这里加了一个小写情况的判定
@@ -54,30 +62,33 @@ def dealwith_headers(r):
     Cache_Control = dealwith_parameter("Cache-Control",r)
     Referer = dealwith_parameter("Referer",r)
     Connection = dealwith_parameter("Connection",r)
-
-    headers = "headers = {\n"+Origin+Accept_Encoding+Accept_Language+Upgrade_Insecure_Requests+User_Agent+Content_Type+Accept+Cache_Control+Referer+Connection+"}"+'\n'
+    authority = dealwith_parameter('Authority',r)
+    x_requested_with = dealwith_parameter('x-requested-with',r)
+    headers = "headers = {\n"+Origin+Accept_Encoding+Accept_Language+Upgrade_Insecure_Requests+\
+              User_Agent+Content_Type+Accept+authority+x_requested_with+Cache_Control+Referer+Connection+"}"+'\n'
     print(headers)
 
 
 
-def main():
+if __name__ == '__main__':
     #输入
     text = str(input("请输入CURL:"))
     print("import requests")
     #尾部无用信息清理
     r = text.replace(" --compressed", "").strip()
     #cookies，data，headers函数调用
-    dealwith_cookies(r)
-    data = []
-    dealwith_data(r,data)
+    cookies = dealwith_cookies(r)
+    data = dealwith_data(r, datas = [])
     dealwith_headers(r)
     #url选取清理
     url = str(re.findall('curl.*?(?:\' )', r)).strip("[]' ").split(" ")[1]
     #由于data字段很少有网页有所包含，所以这里加了一个判断语句用来打印data
-    if  data != []:
+    if data != '[]':
         print("r = requests.post(" + url + ", headers=headers, cookies=cookies, data=data,)")
-    else:
+
+    elif cookies != '[]':
         print("r = requests.post(" + url + ", headers=headers, cookies=cookies,)")
+
+    else:print("r = requests.post(" + url + ", headers=headers,)")
     print("print(r.text)")
 
-main()
